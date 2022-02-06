b25990fde41ed055275bb9673ac53708aa58c254
Joel Carr <joel@joelcarr.co.uk>
2022-02-06 18:22:35 +0000

    Remove CRA demo content and add basic content

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,11 +5,11 @@ import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
+  const { getByRole } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByRole('heading')).toHaveTextContent('Transactions Heatmap');
 });
