24710bd035c5cec5432831e4d9c343abed26b612
Joel Carr <joel@joelcarr.co.uk>
2022-02-07 19:53:10 +0000

    Add reavis to use CalendarHeatmap chart

--- src/features/transactions/Transactions.tsx
+++ src/features/transactions/Transactions.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { CalendarHeatmap } from 'reaviz';
 
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import { getAll, selectTransactions, selectStatus } from './transactionsSlice';
+import {
+  getAll,
+  selectTransactions,
+  selectStatus,
+  Transaction,
+} from './transactionsSlice';
 
 export function Transactions() {
   const transactions = useAppSelector(selectTransactions);
   const status = useAppSelector(selectStatus);
   const dispatch = useAppDispatch();
+
+  function mapDataForChart({ transactionType, amount, date }: Transaction) {
+    return {
+      key: new Date(date),
+      data: transactionType === 'success' ? amount : amount * -1,
+    };
+  }
 
   function handleClick() {
     dispatch(getAll());
@@ -15,7 +28,13 @@ export function Transactions() {
   return (
     <div>
       <button onClick={handleClick}>fetch transactions</button>
-      {status === 'idle' && <p>Transactions lengths: {transactions.length}</p>}
+      {status === 'idle' && transactions.length > 0 && (
+        <CalendarHeatmap
+          data={transactions.map(mapDataForChart)}
+          height={200}
+          width={600}
+        />
+      )}
       {status === 'loading' && <p>Fetching transactions...</p>}
       {status === 'failed' && <p>Fetching transactions failed</p>}
     </div>
