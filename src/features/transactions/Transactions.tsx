3ae1486027e4fc8d934cf53de6e2cbd358d0b353
Joel Carr <joel@joelcarr.co.uk>
2022-02-07 19:27:07 +0000

    Add transaction data and fetch it in app

--- src/features/transactions/Transactions.tsx
+++ src/features/transactions/Transactions.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
-import { useAppSelector } from '../../app/hooks';
-import { selectTransactions } from './transactionsSlice';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { getAll, selectTransactions, selectStatus } from './transactionsSlice';
 
 export function Transactions() {
   const transactions = useAppSelector(selectTransactions);
+  const status = useAppSelector(selectStatus);
+  const dispatch = useAppDispatch();
 
-  return <div>Transactions lengths: {transactions.length}</div>;
+  function handleClick() {
+    dispatch(getAll());
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>fetch transactions</button>
+      {status === 'idle' && <p>Transactions lengths: {transactions.length}</p>}
+      {status === 'loading' && <p>Fetching transactions...</p>}
+      {status === 'failed' && <p>Fetching transactions failed</p>}
+    </div>
+  );
 }
--- src/features/transactions/transactionsSlice.ts
+++ src/features/transactions/transactionsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 
 export interface Transaction {
@@ -17,13 +17,34 @@ const initialState: TransactionsState = {
   status: 'idle',
 };
 
+export const getAll = createAsyncThunk('transactions/getAll', async () => {
+  const response = await fetch('http://localhost:3000/transactions.json').then(
+    (response) => response.json()
+  );
+  return response;
+});
+
 export const transactionsSlice = createSlice({
   name: 'transactions',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAll.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getAll.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.transactions = action.payload;
+      })
+      .addCase(getAll.rejected, (state) => {
+        state.status = 'failed';
+      });
+  },
 });
 
 export const selectTransactions = (state: RootState) =>
   state.transactions.transactions;
+export const selectStatus = (state: RootState) => state.transactions.status;
 
 export default transactionsSlice.reducer;
